c5820e17bf57ec154e784522ed19bd49bae2c9da
Guy <orcabasic@gmail.com>
2022-03-23 11:08:12 +0200

    udated data file

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,32 @@
-import { createStore } from 'vuex'
-// import { boardStore } from './modules/board.store.js'
+import { createStore } from 'vuex';
+import { boardStore } from './modules/board.store.js';
 // import { userStore } from './modules/user.store.js'
 
 // create a store instance
 const store = createStore({
   strict: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+      isLoading: true,
+      isError: false
+  },
+  getters: {
+      isError({ isError }) {
+          return isError
+      }
+  },
+  mutations: {
+      setIsLoading(state, { isLoading }) {
+          state.isLoading = isLoading
+      },
+      setIsError(state, { isError }) {
+          state.isError = isError
+      }
+  },
   actions: {},
   modules: {
-    // toyStore,
+    boardStore,
     // userStore,
   },
-})
+});
 
-export default store
+export default store;
