eef05b2dcf8c5a2c024c0cdac041209909cd53f5
Shiran Elad <shiranelad@gmail.com>
2022-03-26 16:51:25 +0300

    task preview

--- src/components/dynamic-cmps/status-picker.vue
+++ src/components/dynamic-cmps/status-picker.vue
@@ -1,13 +1,13 @@
 <template>
   <section @click="toggleShow" :class="statusClass" class="status-picker" >
-    <div class="status">{{task.cols[0].value}}
+    <div class="status">{{task.cols[0].value === '&nbsp' ? '&nbsp;' : task.cols[0].value}}
     </div>
     <div v-if="menuOpen" class="picker-box">
       <!-- TODO - change back to working on it -->
       <div @click="setStatus('Working on it')" class="working">Working on it</div>
       <div @click="setStatus('Stuck')" class="stuck">Stuck</div>
       <div @click="setStatus('Done')" class="done">Done</div>
-      <div @click="setStatus('null')" class="null">.</div>
+      <div @click="setStatus('&nbsp;')" class="null">&nbsp;</div>
     </div>
   </section>
 </template>
@@ -54,11 +54,13 @@ export default {
   computed: {
     statusClass() {
       if(this.task.cols[0].value === 'Working on it') return 'working'
+      if(this.task.cols[0].value === '&nbsp;') return 'null'
+      // if(this.task.cols[0].value === 'null') return 'null'
       return this.task.cols[0].value.toLowerCase()
     }
   },
   created() {
-    if (!this.task.cols[0].value) this.task.cols[0].value = "status"
+    if (!this.task.cols[0].value) this.task.cols[0].value = ""
   },
   components: {},
 };
