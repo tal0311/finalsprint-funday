b60c8bfeefdb7163857c7249ebdcf9f91d67879a
tal0311 <tal.amit0311@gmail.com>
2022-03-27 10:55:22 +0300

    progress bar tasks

---
+++ src/components/group-progress.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="(status, idx) in tasksStatus" :key="idx">
+      <div>{{ status }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'group-progress',
+
+  data() {
+    return {
+      tasksStatus: [
+        { status: 'done' },
+        { status: 'stuck' },
+        { status: 'done' },
+        { status: 'done' },
+        { status: 'stuck' },
+        { status: 'done' },
+        { status: 'done' },
+        { status: 'working' },
+        { status: 'stuck' },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
