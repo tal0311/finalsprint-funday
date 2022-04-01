f0fa0ab08ebb1c4fa86c529b954754125d18e81b
Shiran Elad <shiranelad@gmail.com>
2022-04-01 16:46:30 +0300

    duplicate board and more css

--- src/components/group/group.vue
+++ src/components/group/group.vue
@@ -20,10 +20,10 @@
         </p>
 
         <div class="col-headers flex">
-          <div class="label status-picker">Status</div>
-          <div class="label member-picker">Person</div>
-          <div class="label date-picker">Date</div>
-          <div class="label priority-picker">Priority</div>
+          <div class="label status-picker" @click="sortTasks('status')">Status</div>
+          <div class="label member-picker" @click="sortTasks('member-picker')">Person</div>
+          <div class="label date-picker" @click="sortTasks('date-picker')">Date</div>
+          <div class="label priority-picker" @click="sortTasks('priority')">Priority</div>
         </div>
       </div>
 
@@ -40,7 +40,7 @@
       :tasks="group.tasks"
       :group="group"
       :groupColor="group.groupColor"
-      style="margin-top:15px"
+      style="margin-top:5px"
     ></tasks-list>
   </section>
 </template>
@@ -66,9 +66,18 @@ export default {
   data() {
     return {
       isOptions: false,
+      ascDesc: 1,
     }
   },
   methods: {
+    sortTasks(col){
+      // let idx ;
+      // if(col==='status') idx = 0
+      // // if(col==='status') idx = 0
+      // this.group.tasks = this.group.tasks.sort((t1,t2) => ((t1.cols[idx].value > t2.cols[idx].value) * this.ascDesc))
+      // this.ascDesc *= -1;
+      console.log(this.group.tasks)
+    },
     addTask(value, groupId) {
       // console.log(value)
       const idx = this.board.groups.findIndex((group) => group.id === groupId)
